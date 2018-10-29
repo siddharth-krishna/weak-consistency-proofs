492f5e628a6ad6a14d616bedf13bb4d7100594d4
Siddharth Krishna <siddharth-krishna@users.noreply.github.com>
2018-10-29 13:43:22 -0400

    violat: add a simple Queue implementation

---
+++ misc/simpleConcurrent/Queue.java
@@ -0,0 +1,61 @@
+package simpleConcurrent;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+public class Queue <E> {
+    AtomicReference<Node<E>> head, tail;
+
+    public Queue() {
+        tail = new AtomicReference<Node<E>>(new Node<E>(null));
+        head = new AtomicReference<Node<E>>(tail.get());
+    }
+
+    public boolean offer(E e) {
+        if (e == null) throw new NullPointerException();
+        final Node<E> newNode = new Node<E>(e);
+
+        Node<E> myTail, myTailNext;
+        while (true) {
+            myTail = tail.get();
+            myTailNext = myTail.next.get();
+
+            if (myTailNext == null) {
+                if (myTail.next.compareAndSet(myTailNext, newNode))
+                    return true;
+            } else {  // Try to advance tail
+                tail.compareAndSet(myTail, myTailNext);
+            }
+        }
+    }
+
+    public E poll() {
+        E val;
+
+        while (true) {
+            Node<E> myHead = head.get();
+            Node<E> myTail = tail.get();
+            Node<E> myHeadNext = myHead.next.get();
+
+            if (myHead == myTail) {
+                if (myHeadNext == null) {
+                    return null;
+                }
+                tail.compareAndSet(myTail, myHeadNext);  // Try to advance tail
+            } else {
+                val = myHeadNext.item;
+                if (head.compareAndSet(myHead, myHeadNext))
+                    return val;
+            }  // Else, try again from top
+        }
+    }
+
+    private static class Node <E> {
+        public final E item;
+        public AtomicReference<Node<E>> next;
+
+        public Node(E item) {
+            this.item = item;
+            this.next = new AtomicReference<Node<E>>();
+        }
+    }
+}
